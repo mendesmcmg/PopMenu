78d66c40ab2ce6bc323d72b075e6d9b48aac11c0
mendesmcmg <claraplanejamento@gmail.com>
2024-10-21 18:13:55 -0300

    test: adds tests for models

--- test/models/menu_item_test.rb
+++ test/models/menu_item_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class MenuItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @restaurant_menu = RestaurantMenu.create(name: "Dinner Menu")
+    @menu_item = @restaurant_menu.menu_items.build(name: "Spaghetti", price: 12.99, category: "Pasta", available: true)
+  end
+
+  test "should be valid" do
+    assert @menu_item.valid?
+  end
+
+  test "should set attributes correctly" do
+    assert_equal @menu_item.name, "Spaghetti"
+    assert_equal @menu_item.price, 12.99
+    assert_equal @menu_item.category, "Pasta"
+    assert_equal @menu_item.available, true
+  end
+
+  test "menu item should belong to a restaurant menu" do
+    assert_equal @restaurant_menu, @menu_item.restaurant_menu
+  end
 end
--- test/models/restaurant_menu_test.rb
+++ test/models/restaurant_menu_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class RestaurantMenuTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @restaurant_menu = RestaurantMenu.new(name: "Lunch Menu", description: "A nice meal for your day :)", active: true)
+  end
+
+  test "should be valid" do
+    assert @restaurant_menu.valid?
+  end
+
+  test "should set attributes correctly" do
+    assert_equal @restaurant_menu.name, "Lunch Menu"
+    assert_equal @restaurant_menu.description, "A nice meal for your day :)"
+    assert_equal @restaurant_menu.active, true
+  end
+
+  test "should have many menu items" do
+    assert_respond_to @restaurant_menu, :menu_items
+  end
 end
